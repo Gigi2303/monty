8dfd5bd09fdaae1bc04ef6b58cbd099a6c95125d
emmachinko <emmachris4603@gmail.com>
2023-12-14 11:36:32 -0800

    C - Stacks, Queues - LIFO, FIFO

---
+++ pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+  
+/**
+ * f_pall - pushes an element to the stack
+ * @head: stack head
+ * @counter: NULL
+ * return: NULL
+ */
+void f_pall(stack_t **head, unsign int counter)
+{
+	stack_t *h;
+	counter(void);
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d/n", h->n);
+		h = h->next;
+	}
+}
